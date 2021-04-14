0d77125f0d0612bf9b2d30614dd96617c97a301d
marijal74 <lakic.marija98@gmail.com>
2021-04-14 11:56:27 +0200

    feat/main Run PandemicShop

--- source/dagger/main.cpp
+++ source/dagger/main.cpp
@@ -5,13 +5,15 @@
 #include "gameplay/racing/racing_main.h"
 #include "gameplay/team_game/team_game_main.h"
 #include "gameplay/tiles_example/tiles_example_main.h"
+#include "gameplay/PandemicShop/pandemic_shop_main.h"
 
 int main(int argc_, char** argv_)
 {
 	dagger::Engine engine;
-//	return engine.Run<tiles_example::TilesExampleMain>();
-//	return engine.Run<team_game::TeamGame>();
-//	return engine.Run<ping_pong::PingPongGame>();
-//	return engine.Run<racing_game::RacingGame>();
-	return engine.Run<platformer::Platformer>();
+	// return engine.Run<tiles_example::TilesExampleMain>();
+	//	return engine.Run<team_game::TeamGame>();
+	// return engine.Run<ping_pong::PingPongGame>();
+	//	return engine.Run<racing_game::RacingGame>();
+	// return engine.Run<platformer::Platformer>();
+	return engine.Run<pandemic_shop::PandemicShopGame>();
 }
